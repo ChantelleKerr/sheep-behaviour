fc8fda25030cfe49ea5be40b80cab4a8d5180d43
ChantelleKerr <chantellekerr93@gmail.com>
2023-08-09 14:18:30 +0800

    remove print statements

--- sandbox/cluster.py
+++ sandbox/cluster.py
@@ -5,24 +5,22 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 
+# Would be better to use tensorflow instead of sklearn
 input_data = pd.read_csv("data/cleaned.csv")
 
 input_data = input_data.drop(columns=["seconds"])
-print("1")
+
 
 n_clusters = 4
 kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-print("2")
+
 
 data = input_data[3552400:3552700]
 kmeans.fit(data)
-print("3")
 
 cluster_labels = kmeans.labels_
-print("4")
 
 data["Cluster"] = cluster_labels
-print("5")
 
 
 
@@ -39,16 +37,6 @@ plt.title("Cluster Distribution Over Time")
 plt.legend()
 plt.show()
 
-# # Create an interactive plot using Plotly
-# fig = px.scatter(data, x=data.index / 25, y="Cluster", color="Cluster",
-#                  labels={"x": "Time (seconds)", "y": "Cluster"},
-#                  title="Cluster Distribution Over Time",
-#                  range_x=[data.index[0] / 25, data.index[-1] / 25],
-#                  animation_frame=data.index / 25,
-#                  animation_group="Cluster")
-
-# # Show the plot
-# fig.show()
 
 
 
