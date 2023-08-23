e9c718af66524a1f486b09d5500b11155e668169
Karla Ivkovic <karla-ivkvc@users.noreply.github.com>
2023-08-23 12:09:43 +0800

    Tkinter read file

--- sandbox/gui.py
+++ sandbox/gui.py
@@ -5,6 +5,11 @@ def openFile():
     filepath = filedialog.askopenfilename()
     print(filepath)
 
+    file = open(filepath, 'r') #reading file 
+    print(file.read())
+    file.close()
+
+
 window = Tk()
 button = Button(text="Open", command=openFile)
 button.pack()
