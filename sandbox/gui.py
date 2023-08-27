b7f719a2ac3d157ee5391115b681a38bf9e65b23
Karla Ivkovic <karla-ivkvc@users.noreply.github.com>
2023-08-27 13:06:09 +0800

    Clean data button - saves new file in the same dir

--- sandbox/gui.py
+++ sandbox/gui.py
@@ -2,24 +2,39 @@ from tkinter import *
 from tkinter import filedialog
 import sys
 
+filepath = None
+
 def openFile():
+    global filepath
     filepath = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
     if not filepath:
         return
 
     try: 
         file = open(filepath, 'r') #reading file 
-        cleanFileButton(file)
+        window.title(f"Simple Text Editor - {filepath}")
+        clean_file_button["state"] = NORMAL
 
         file.close()
     except:
         print("Could not open file:", filepath)
         sys.exit()
 
-def cleanFileButton(file):
-    clean_file_button["state"] = NORMAL
-    # reading from the file
-    print(file.read())
+def cleanFile():
+    print(filepath)
+    directory = filepath.rsplit("/", 1)
+    print(directory[0])
+
+    try:
+        clean_filename = directory[0]+"/cleanfile.txt"
+        print(clean_filename)
+        f=open(clean_filename,'w') 
+        f.write("The contents of this file...") 
+        f.close()
+        print("Clean file saved successfully.")
+    except:
+        print("Could not save file")
+        sys.exit()
 
 window = Tk()
 
@@ -33,7 +48,7 @@ menu_frame.grid(row=0, column=0, sticky="ns")
 load_file_button = Button(menu_frame, text='Load File', command=openFile) 
 load_file_button.grid(row=0, column=0, sticky="ew",padx=(5), pady=(5))
 
-clean_file_button = Button(menu_frame, text='Clean File', state=DISABLED, command=cleanFileButton)
+clean_file_button = Button(menu_frame, text='Clean File', state=DISABLED, command=cleanFile)
 clean_file_button.grid(row=1, column=0, sticky="ew", padx=(10))
 
 window.mainloop()
