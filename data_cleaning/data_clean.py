478cdf5e47431db00375f00d18c437289bd689e9
ChantelleKerr <chantellekerr93@gmail.com>
2023-08-28 01:11:36 +0800

    calculate dates

--- data_cleaning/data_clean.py
+++ data_cleaning/data_clean.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 import numpy as np
@@ -11,57 +12,66 @@ class ProcessData():
         """
         dfs = []
         file_names = sorted(os.listdir(folder_path))
-        is_first = True  # Start by assuming the first file has a header
-        header_count = 0
-
+        header = None  # Initialize header to None
 
         for file in file_names:
-            print(file)
+            print(f'Processing: {file}')
             if file.endswith('.txt'):
                 file_path = os.path.join(folder_path, file)
-                skiprows = 1 if is_first else None
-                df = pd.read_csv(file_path, usecols=range(3), header=None, skiprows=skiprows)
-                # dfs.append(df)
-                df_array = df.to_numpy()
-                print(df)
-                is_first = False
 
-                header_indices = np.where(np.all(df_array[:, :1] == "ACCEL_X", axis=1))[0].tolist()
-
-                if header_indices:
-                    header_count += 1
-            
-                if header_count <= 2:
-                    last_row_index = header_indices[0] if header_indices else None
-                    if last_row_index is not None:
-                        dfs.append(df.iloc[:last_row_index])
-                    else:
-                        dfs.append(df)
-            
-            
+                if header is None:
+                    df = pd.read_csv(file_path)
+                    header = df.columns.tolist()
+                else:
+                    df = pd.read_csv(file_path, header=None, names=header)
+                dfs.append(df)
+                
         combined_df = pd.concat(dfs, ignore_index=True)
         return combined_df
     
-    # TODO : Handle different Hz, 
-    #       convert time column to HH:MM:SS and add date column (or should this be combined?) - the acutal time and date needs to start from a given time and date.
-    #       remove -2048, -2048,-2048 rows
-    
     
     def clean_data(self, df):
-        mask = df.iloc[:, 0].str.startswith('*')
-        indices = df.loc[mask, df.columns[0]].str[1:].astype(int).ffill().fillna(0).astype(int)
-        print(indices)
-        df['seconds'] = indices
-        df['seconds'] = df['seconds'].fillna(method='ffill').astype(int)
+        # Find the first row with no NaN values (This is where the first date and time is recorded)
+        first_valid_row = df.dropna().iloc[0]
 
-
-        df = df.loc[~mask].reset_index(drop=True)
-        print(df)
+        # Extract the date values and format it for calculations
+        columns_to_extract = ['DAY', 'MONTH', 'YEAR', 'HOUR', 'MINUTE', 'SECOND']
+        values_of_first_valid_row = first_valid_row[columns_to_extract].astype(int).values
+        datetime_format = '%d %m %Y %H %M %S'
+        datetime_str = ' '.join(map(str, values_of_first_valid_row))
+        datetime_obj = pd.to_datetime(datetime_str, format=datetime_format)
         
-        #zero_mask = (df.iloc[:, 0] == 0) & (df.iloc[:, 1] == 0) & (df.iloc[:, 2] == 0)
-    
-        #zero_indices = zero_mask.index[zero_mask]
-   
+
+        print("First Date:",datetime_obj)
+
+        # Find the current overall second at the date and time above
+        index_of_first_valid_row = first_valid_row.name
+        seconds_at_first_valid_date = df.iloc[index_of_first_valid_row + 1]['ACCEL_X']
+        seconds_at_first_valid_date = int(seconds_at_first_valid_date.replace('*', ''))
+        print("Current Second:", seconds_at_first_valid_date - 1) # Minus 1 because this is the next second (we want the previous so it matches the date above)
+
+        # Calculate the initial start date
+        initial_datetime = datetime_obj - pd.Timedelta(seconds=seconds_at_first_valid_date)
+        print("INITIAL DATE:", initial_datetime)
+
+        df = df.drop(columns=['LAT', 'LON', 'DAY', 'MONTH', 'YEAR', 'HOUR', 'MINUTE', 'SECOND'])
+        
+        # Calculate datetime
+        date_column = np.empty(len(df), dtype=object)
+        current_datetime = initial_datetime
+        for index, row in df.iterrows():
+            date_column[index] = current_datetime.strftime('%d/%m/%Y %H:%M:%S')
+            
+            if row["ACCEL_X"].startswith("*") and index != 0:
+                current_datetime += pd.Timedelta(seconds=1)
+
+        # Assign the date_column to the DataFrame
+        df['DATE'] = date_column
+
+        # Remove rows starting with "*"
+        mask = df.iloc[:, 0].str.startswith('*') 
+        df = df[~mask]
+
         return df
         
     def save_to_csv(self, df, file_name):
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import time
+
 from data_cleaning.data_clean import ProcessData
+
+#from data_cleaning.multiprocess import ProcessData
+
+
 
 ## Application starting point
 ## Run python3 main.py or python main.py
 if __name__ == "__main__":
+    start_time = time.time()
     process_data = ProcessData()
-    folder_path = "test_data/sheep1"  # didnt save data files in repo its too large (youll need to add your own datafile)
+    folder_path = "test_data/sheep1"  # hardcoded data path
     combined_data = process_data.read_data(folder_path)
+   
     cleaned_data = process_data.clean_data(combined_data)
-    #print(cleaned_data)
+    
+    print(cleaned_data)
     process_data.save_to_csv(cleaned_data, "test_data/sheep1/cleaned.csv") 
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
