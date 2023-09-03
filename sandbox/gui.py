7fb78d27b1de50cc04a6101063924636dfb9e3d2
Karla Ivkovic <karla-ivkvc@users.noreply.github.com>
2023-09-03 21:11:59 +0800

    added multiple files selection

--- sandbox/gui.py
+++ sandbox/gui.py
@@ -2,39 +2,53 @@ from tkinter import *
 from tkinter import filedialog
 import sys
 
-filepath = None
+filepaths = None
+cleanedFile = None
 
-def openFile():
-    global filepath
-    filepath = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
-    if not filepath:
+def openFiles():
+    global filepaths
+    fps = filedialog.askopenfilenames(filetypes=[("CSV Files", "*.csv")])
+    if not fps:
         return
 
-    try: 
-        file = open(filepath, 'r') #reading file 
-        window.title(f"Sheep File - {filepath}")
-        clean_file_button["state"] = NORMAL
+    filepaths = list(fps)
 
-        file.close()
-    except:
-        print("Could not open file:", filepath)
-        sys.exit()
+    label = Label(second_frame, text="Successfully loaded:", font=("Helvetica", 18)) 
+    label.grid(row=0, column=0, sticky="ew")
 
-def cleanFile():
-    print(filepath)
-    directory = filepath.rsplit("/", 1)
+    increment = 0
+    for filepath in filepaths:
+        try:
+            label = Label(second_frame, text=filepath) 
+            label.grid(row=1+increment, column=0, sticky="ew")
+            increment = increment+1
+            clean_file_button["state"] = NORMAL
+        except:
+            print("Could not open file:", filepath)
+            sys.exit()
+
+def cleanFiles():
+    global cleanedFile
+    # Get the directory of the selected files:
+    directory = filepaths[0].rsplit("/", 1)
     print(directory[0])
 
-    try:
-        clean_filename = directory[0]+"/cleanfile.txt"
-        print(clean_filename)
-        f=open(clean_filename,'w') 
-        f.write("The contents of this file...") 
-        f.close()
-        print("Clean file saved successfully.")
-    except:
-        print("Could not save file")
-        sys.exit()
+    for filepath in filepaths:
+        try:
+            print(filepath)
+            file = open(filepath, 'r') #Reading file
+
+            # DO SOMETHING WITH OPENED FILE
+            ###############################
+            cleanedFile = "Hello World"
+        except:
+            sys.exit()
+
+    clean_filename = directory[0]+"/cleanfile.txt"
+    print(clean_filename)
+    f=open(clean_filename,'w') 
+    f.write(cleanedFile) 
+    f.close()
 
 window = Tk()
 
@@ -45,10 +59,13 @@ window.columnconfigure(1,minsize=800, weight=1)
 menu_frame = Frame(window, relief=RAISED, bd=2, bg="gray98")
 menu_frame.grid(row=0, column=0, sticky="ns")
 
-load_file_button = Button(menu_frame, text='Load File', command=openFile) 
+second_frame = Frame(window)
+second_frame.grid(row=0,column=1)
+
+load_file_button = Button(menu_frame, text='Load Files', command=openFiles) 
 load_file_button.grid(row=0, column=0, sticky="ew",padx=(5), pady=(5))
 
-clean_file_button = Button(menu_frame, text='Clean File', state=DISABLED, command=cleanFile)
+clean_file_button = Button(menu_frame, text='Clean Files', state=DISABLED, command=cleanFiles)
 clean_file_button.grid(row=1, column=0, sticky="ew", padx=(10))
 
 window.mainloop()
