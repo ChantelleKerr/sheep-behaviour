05e3b0395e9232dee0c1b6f1149b1f687ec8c50a
JakeScottHill <112530311+JakeScottHill@users.noreply.github.com>
2023-09-29 17:48:50 +0800

    Formatting Changes
    Made some formatting changes to the frontend for windows for now, I tried to use grid mostly so it's hopefully scalable to mac but we will see.

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ global_var_lock = threading.Lock()
 
 def getFolders(folder_paths):
     folder_paths.append(askopendirnames())
-    print(folder_paths)
 
     for tuple in folder_paths:
         for folder_path in tuple:
@@ -38,8 +37,8 @@ def getFolders(folder_paths):
                 messagebox.showerror("Error", "Folder: "+folder_path+" contains less than 2 files. Please select a folder which contains more than one data file.")
                 folder_paths.pop()
                 break
-
-    if len(folder_paths) == 1:
+    
+    if len(folder_paths) == 1 and folder_paths[0] != ():
         load_files_button["state"] = DISABLED
         clean_files_button["state"] = NORMAL
     return folder_paths
@@ -215,7 +214,7 @@ if __name__ == "__main__":
 
 
     #Data Processing
-    Label(menu_frame,  text="Data Processing", bg='#27348b', fg='white', font="Arial 16").grid(row=0, column=0, padx=25, pady=10)
+    Label(menu_frame,  text="Data Processing", bg='#27348b', fg='white', font="Arial 18").grid(row=0, column=0, padx=20, pady=10)
     load_files_button = Button(menu_frame, text="LOAD DIRECTORY", font="Arial 14 bold", background='#fdc300', activebackground='#fdc300', focuscolor='', borderless=True, padx=5, pady=15, command = lambda: getFolders(folder_paths))
     clean_files_button = Button(menu_frame, text="CLEAN DIRECTORY", font="Arial 14 bold", background='#a2c03b', activebackground='#a2c03b', focuscolor='', borderless=True, state=DISABLED, padx=0, pady=15, command = lambda: unthreaded_clean_files(read_pb, clean_pb, write_pb, root, folder_paths))
     load_files_button.grid(row=1, column=0, rowspan=2)
@@ -229,13 +228,13 @@ if __name__ == "__main__":
     #Data Analysis
     Label(menu_frame,  text="Data Analysis", bg='#27348b', fg='white', font="Arial 16").grid(row=6, column=0, padx=25, pady=5)
     
-    Label(menu_frame,  text="Start Date", bg='#27348b', justify="left", anchor="w", fg='white', font="Arial 12").grid(sticky = W, row=7, column=0, padx=10, pady=0)
+    Label(menu_frame,  text="Start Date:", bg='#27348b', justify="left", anchor="w", fg='white', font="Arial 12").grid(sticky = W, row=7, column=0)
     start_date = DateEntry(menu_frame, background='#27348b', selectmode='day', date_pattern='yyyy-MM-dd')
     start_date._top_cal.overrideredirect(False)
-    start_date.grid(row=8, column=0)
+    start_date.grid(row=7, column=0, padx=(60,0))
 
-    Label(menu_frame,  text="Start Time", bg='#27348b', fg='white', font="Arial 12").grid(sticky = W, row=9, column=0, padx=0, pady=2)
-    Label(menu_frame,  text='', bg='#27348b', fg='white', font="Arial 12").grid(sticky = E, row=10, column=0, padx=0, pady=3) #filler label
+    Label(menu_frame,  text="Start Time:", bg='#27348b', fg='white', font="Arial 12").grid(sticky = W, row=9, column=0, pady=(10))
+    #Label(menu_frame,  text='', bg='#27348b', fg='white', font="Arial 12").grid(sticky = E, row=10, column=0, padx=0, pady=3) #filler label
     
     hours_list = []
     minutes_list = []
@@ -254,35 +253,32 @@ if __name__ == "__main__":
     start_hour.set("Hour")
     start_hours = ttk.Combobox(menu_frame, textvariable=start_hour, state="readonly", values=hours_list, width=5)
     start_hours.bind("<FocusIn>", defocus)
-    start_hours.place(x=15, y=275)
-    
-    colon1 = Label(menu_frame,  text=":", bg='#27348b', fg='white', font="Arial 12")
-    colon1.place(x=86, y=275)
+    start_hours.grid(row=9, column=0, padx=(20, 0))
 
     start_minute = StringVar(menu_frame)
     start_minute.set("Mins")
     start_minutes = ttk.Combobox(menu_frame, textvariable=start_minute, state="readonly", values=minutes_list, width=5)
     start_minutes.bind("<FocusIn>", defocus)
-    start_minutes.place(x=98, y=275)
+    start_minutes.grid(row=9, column=0, padx=(150, 0))
 
-    Label(menu_frame,  text="End Date", bg='#27348b', justify="left", anchor="w", fg='white', font="Arial 12").grid(sticky = W, row=11, column=0, padx=10, pady=0)
+    Label(menu_frame,  text="End Date:", bg='#27348b', justify="left", anchor="w", fg='white', font="Arial 12").grid(sticky = W, row=13, column=0, pady=(20, 0))
     end_date = DateEntry(menu_frame, background='#27348b', selectmode='day', date_pattern='yyyy-MM-dd')
     end_date._top_cal.overrideredirect(False)
-    end_date.grid(row=12, column=0)
+    end_date.grid(row=13, column=0, padx=(60, 0), pady=(20, 0))
 
-    Label(menu_frame,  text="End Time", bg='#27348b', fg='white', font="Arial 12").grid(sticky = W, row=13, column=0, padx=0, pady=2)
+    Label(menu_frame,  text="End Time:", bg='#27348b', fg='white', font="Arial 12").grid(sticky = W, row=14, column=0, pady=(10, 0))
     end_hour = StringVar(menu_frame)
     end_hour.set("Hour")
     end_hours = ttk.Combobox(menu_frame, textvariable=end_hour, state="readonly", values=hours_list, width=5)
-    end_hours.place(x=15, y=380)
+    end_hours.grid(row=14, column=0, padx=(20, 0))
 
-    colon2 = Label(menu_frame,  text=":", bg='#27348b', fg='white', font="Arial 12")
-    colon2.place(x=86, y=380)
+    # colon2 = Label(menu_frame,  text=":", bg='#27348b', fg='white', font="Arial 12")
+    # colon2.grid(row=14, column=0, padx=(150, 0))
 
     end_minute = StringVar(menu_frame)
     end_minute.set("Mins")
     end_minutes = ttk.Combobox(menu_frame, textvariable=end_minute, state="readonly", values=minutes_list, width=5)
-    end_minutes.place(x=98, y=380)
+    end_minutes.grid(row=14, column=0, padx=(150, 0))
 
     Label(menu_frame,  text='', bg='#27348b', fg='white', font="Arial 12").grid(sticky = E, row=14, column=0, padx=0, pady=2) #filler label
 
@@ -291,28 +287,28 @@ if __name__ == "__main__":
     canvas2.create_line(5, 25, 165, 25, width=0, fill='white')
     canvas2.grid(row=15, column=0)
 
-    select_sheep_button = Button(menu_frame, text="SELECT SHEEP", font="Arial 14 bold", background='#fdc300', activebackground='#fdc300', focuscolor='', borderless=True, padx=10, pady=15, command=selectSheep)    
-    start_analysis_button = Button(menu_frame, text="START ANALYSIS", font="Arial 14 bold", background='#a2c03b', activebackground='#a2c03b', focuscolor='', borderless=True, state=DISABLED, padx=5, pady=15, command= lambda: startAnalysis(start_date.get_date(), end_date.get_date(), start_hours.get(), start_minutes.get(), end_hours.get(), end_minutes.get()))
+    select_sheep_button = Button(menu_frame, text="SELECT SHEEP", font="Arial 14 bold", background='#fdc300', activebackground='#fdc300', focuscolor='', borderless=True, padx=15, pady=15, command=selectSheep)    
+    start_analysis_button = Button(menu_frame, text="START ANALYSIS", font="Arial 14 bold", background='#a2c03b', activebackground='#a2c03b', focuscolor='', borderless=True, state=DISABLED, padx=7, pady=15, command= lambda: startAnalysis(start_date.get_date(), end_date.get_date(), start_hours.get(), start_minutes.get(), end_hours.get(), end_minutes.get()))
     select_sheep_button.grid(row=16, column=0, rowspan=2)
     start_analysis_button.grid(row=18, rowspan=2, column=0)
     
     #Logo
     uwa_logo = Image.open("./UWA-logo-1.png")
-    img_resized=uwa_logo.resize((200,100)) # new width & height
+    img_resized=uwa_logo.resize((220,80)) # new width & height
     my_img=ImageTk.PhotoImage(img_resized)
 
     l1=Label(menu_frame,image=my_img,background='#27348b')
-    l1.place(rely=1.0, relx=1.0, x=-85, y=-10, anchor=S)
+    l1.grid(column=0, pady=(50, 0))
 
     #Graph labels and buttons
-    Label(graph_frame,  text="Current Directory", fg='#27348b', font="Arial 12 bold").grid(row=0, column=0, padx=2, pady=5, rowspan=2)
-    Label(graph_frame,  text="", fg='black', font="Arial 12").grid(sticky = W, row=0, column=1, padx=2, pady=5, rowspan=2)
-    Label(graph_frame,  text="Current Mode", fg='#27348b', font="Arial 12 bold").grid(sticky = W, row=3, column=0, padx=2)
-    Label(graph_frame,  text="", fg='black', font="Arial 12").grid(sticky = W, row=3, column=1, padx=2, pady=5, rowspan=2)
+    Label(graph_frame,  text="Current Directory", fg='#27348b', font="Arial 12 bold").grid(sticky=W, padx=(10), pady=(10, 5), rowspan=2)
+    Label(graph_frame,  text="", fg='black', font="Arial 12").grid(sticky = W, row=0, column=1,rowspan=2)
+    Label(graph_frame,  text="Current Mode", fg='#27348b', font="Arial 12 bold").grid(sticky = W, row=3, column=0, padx=(10))
+    Label(graph_frame,  text="", fg='black', font="Arial 12").grid(sticky = W, row=3, column=1, padx=2, rowspan=2)
 
     export_pdf_button = Button(graph_frame, text="EXPORT TO PDF", font="Arial 12", background='#27348b', activebackground='#fdc300', fg='white', focuscolor='', borderless=True, padx=5, pady=10)
     generate_report_button = Button(graph_frame, text="GENERATE REPORT", font="Arial 12", background='#fdc300', activebackground='#a2c03b', focuscolor='', borderless=True, padx=0, pady=10)
-    export_pdf_button.place(x=300, y=500)
-    generate_report_button.place(x=450, y=500)
+    export_pdf_button.place(rely=1.0, relx=1.0, x=-250, y=-30, anchor=SE)
+    generate_report_button.place(rely=1.0, relx=1.0, x=-70, y=-30, anchor=SE)
 
     root.mainloop()
