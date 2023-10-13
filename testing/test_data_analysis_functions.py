b1de0468868c166f75dafe22e75f638fe61c8dae
JakeScottHill <112530311+JakeScottHill@users.noreply.github.com>
2023-10-14 03:09:25 +0800

    Added Testing for Start and End Data

--- testing/test_data_analysis_functions.py
+++ testing/test_data_analysis_functions.py
@@ -1,12 +1,21 @@
 import unittest
+import pandas as pd
+from pandas.testing import assert_frame_equal
 from data_analysis.plot import AnalyseSheep
+
+sampleDataFile = "testing/test_data.csv"
+expectedData = pd.DataFrame({'ACCEL_X': [236, 223, 262], 'ACCEL_Y': [28, 56, 12], 'ACCEL_Z': [471, 503, 497], 'DATE': [pd.to_datetime("2023-02-15 08:03:12"), pd.NaT, pd.to_datetime("2023-03-02 10:09:12")]})
+sampleStartDate = "2023-02-15 08:03:12"
+sampleEndDate   = "2023-03-02 10:09:12"
 
 class TestDataAnalysis(unittest.TestCase):
     
-    def test_data_analysis():
-        
-        AnalyseSheep.find_start_and_end_data
-        
+    def setUp(self):
+        analyseSheep = AnalyseSheep()
+        self.date = analyseSheep.find_start_and_end_data(sampleDataFile, sampleStartDate, sampleEndDate)
+    
+    def test_start_and_end_data(self):
+        assert_frame_equal(self.date, expectedData, "Start and end data location error")
 
 if __name__ == "__main__":
     unittest.main()
