25917bf7e59b32bd57ff2fee4cd7de7e31923238
JakeScottHill <112530311+JakeScottHill@users.noreply.github.com>
2023-10-15 12:37:13 +0800

    Updated test_app_running for Mac and Windows

--- testing/test_app_running.py
+++ testing/test_app_running.py
@@ -1,16 +1,40 @@
 import unittest
 import psutil
+import platform
+
+system = platform.system()
 
 class TestRunningStatus(unittest.TestCase):
 
     # Checks if the application is open and running.
     def test_app_running(self):
         running = False
-        processCMDLine = ['c:/Users/jakes/Programs/sheep-behaviour/venv/Scripts/python.exe', 'c:/Users/jakes/Programs/sheep-behaviour/main.py']
-        for process in psutil.process_iter(attrs=['cmdline']):
-            if process.info['cmdline'] == processCMDLine:
-                running = True
-                break
+        processDevCMDLine = "sheep-behaviour/main.py"
+        processAppCMDLine = ""
+        if system == "Darwin":
+            processAppCMDLine = "App/Mac/main.exe"
+        elif system == "Windows":
+            processAppCMDLine = "App/Windows/main.exe"
+        
+        for process in psutil.process_iter(attrs=['name', 'cmdline']):
+            CMDInfo = process.info['cmdline']
+            
+            if CMDInfo is not None and len(CMDInfo) > 0:
+                if system == "Darwin":
+                    if process.info['name'] == "main.exe" and ('/'.join(CMDInfo[0].rsplit("/", 3)[1:])) == processAppCMDLine:
+                        running = True
+                        break
+                    elif ('/'.join(CMDInfo[-1].rsplit("/", 2)[1:])) == processDevCMDLine:
+                        running = True
+                        break
+                elif system == "Windows":
+                    if process.info['name'] == "main.exe" and ('/'.join(CMDInfo[0].rsplit("\\", 3)[1:])) == processAppCMDLine:
+                        running = True
+                        break
+                    elif ('/'.join(CMDInfo[-1].rsplit("/", 2)[1:])) == processDevCMDLine:
+                        running = True
+                        break
+                    
         
         self.assertTrue(running, 'Application not running as expected')
 
